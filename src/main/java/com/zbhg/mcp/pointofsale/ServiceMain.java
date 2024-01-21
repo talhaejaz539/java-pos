f1b64349972f0baa6ba14ca62ab200164280c0ab
talhaejaz539 <talhaejazup@gmail.com>
2024-01-21 11:29:55 +0500

    ServiceMain Updated

--- src/main/java/com/zbhg/mcp/pointofsale/ServiceMain.java
+++ src/main/java/com/zbhg/mcp/pointofsale/ServiceMain.java
@@ -5,6 +5,7 @@ import com.zbhg.mcp.pointofsale.service.MainController;
 
 public class ServiceMain {
     public static void main(String[] args) {
+        System.out.println("21 January 2024 11:30 AM");
         MainController mainController = new MainController();
         try {
             mainController.start();
